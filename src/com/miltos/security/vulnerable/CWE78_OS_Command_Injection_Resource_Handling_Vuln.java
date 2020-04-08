c4e0c7e1a39017aef05a66ea16e7b51d71810474
siavvasm <siavvasm@iti.gr>
2020-04-08 17:10:20 +0300

    New Commit

--- src/com/miltos/security/vulnerable/CWE78_OS_Command_Injection_Resource_Handling_Vuln.java
+++ src/com/miltos/security/vulnerable/CWE78_OS_Command_Injection_Resource_Handling_Vuln.java
@@ -1,17 +1,8 @@
 /* TEMPLATE GENERATED TESTCASE FILE
-Filename: CWE78_OS_Command_Injection__File_08.java
+Filename: CWE78_OS_Command_Injection__File_01.java
 Label Definition File: CWE78_OS_Command_Injection.label.xml
-Template File: sources-sink-08.tmpl.java
+Template File: sources-sink-01.tmpl.java
 */
-/*
-* @description
-* CWE: 78 OS Command Injection
-* BadSource: File Read data from file (named c:\data.txt)
-* GoodSource: A hardcoded string
-* BadSink: exec dynamic command execution with Runtime.getRuntime().exec()
-* Flow Variant: 08 Control flow: if(privateReturnsTrue()) and if(privateReturnsFalse())
-*
-* */
 /*
 * Modified by: Miltiadis Siavvas
 */
@@ -22,30 +13,20 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Collection;
 import java.util.Iterator;
+import java.util.Vector;
 import java.util.stream.Stream;
 
-public class CWE78_OS_Command_Injection_File_08_Vuln {
+public class CWE78_OS_Command_Injection_Resource_Handling_Vuln {
 	
 	final static String DATA = new File("C:\\Users\\siavvasm.ITI-THERMI.000\\Desktop\\input_data.txt").getAbsolutePath();
-	
-    /* The methods below always return the same value, so a tool
-     * should be able to figure out that every call to these
-     * methods will return true or return false.
-     */
-    private static boolean privateReturnsTrue() throws Exception
-    {
-        return true;
-    }
 
-    private static boolean privateReturnsFalse() throws Exception
-    {
-        return false;
-    }
-    
-	public static void bad(String param) throws Exception {
+	public static void bad(final String param) throws Exception {
+		
 		//TODO: Remove this print 
-		System.out.println("Inside BAD method");
+		System.out.println("*** Start of potentially vulnerable method ***");
+		
 		// 1. Define the data of the command
 		String data;
 		data = param;
@@ -65,25 +46,17 @@ public class CWE78_OS_Command_Injection_File_08_Vuln {
         
         // 3. Execute the command
         /* POTENTIAL FLAW: command injection */
-        if(privateReturnsTrue()) {
-            data = data + " true";
-        } else {
-        	data = data + " false";
-        }
-
-        Process process = Runtime.getRuntime().exec(osCommand + data);
+        final Process process = Runtime.getRuntime().exec(osCommand + data);
         process.waitFor();
         osCommand = null;
         data = null;
-        param = null;
-
         
 		//TODO: Remove this print 
-		System.out.println("End of BAD method");
+		System.out.println("*** End of potentially vulnerable method ***");
 		
 	}
 	
-    public static void good(String param) throws Exception {
+    public static void good(final String param) throws Exception {
     	
     	// 1. Read the parameter
         String data = param;
@@ -102,19 +75,19 @@ public class CWE78_OS_Command_Injection_File_08_Vuln {
         }
 
         /* POTENTIAL FLAW: command injection */
-        Process process = Runtime.getRuntime().exec(osCommand + data);
+        final Process process = Runtime.getRuntime().exec(osCommand + data);
         process.waitFor();
 
     }
     
-	public static void main(String[] args) throws Exception {
+	public static void main(final String[] args) throws Exception {
 		
 		//TODO: Remove this print
-		System.out.println("This is a dummy class!!");
+		System.out.println("*** Executing an application that is vulnerable to OS Command Injection ...");
 		
 		// 1. Open the desired file
-		FileReader fr = new FileReader(DATA);
-		BufferedReader br = new BufferedReader(fr);
+		final FileReader fr = new FileReader(DATA);
+		final BufferedReader br = new BufferedReader(fr);
 		
 		// 2. Read the user defined parameters from the desired file
 		Stream<String> parameters = br.lines();
@@ -122,8 +95,16 @@ public class CWE78_OS_Command_Injection_File_08_Vuln {
 		
 		String parameter = "";
 		
+		Collection c1 = new Vector();
+		Collection c2 = new Vector();
+
+		
 		// 3. Execute the command for each parameter
 		while(parameterIt.hasNext()) {
+			
+			Object obj = new Object();
+			String str = new String();
+			String str2 = new String();
 			
 			// Read the parameter
 			parameter = parameterIt.next();
@@ -132,10 +113,12 @@ public class CWE78_OS_Command_Injection_File_08_Vuln {
 			if(!parameter.equals(null)){
 				bad(parameter);
 			}
-			
+
 			//TODO: Remove this print
 			System.out.println(parameter);
 		}
+		
 	}
+
 
 }
